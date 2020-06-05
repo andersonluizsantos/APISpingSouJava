70332c81f0ad125fb9ff5530b14d0c772a712ad1
Anderson Santos <andersonluizsantos@gmail.com>
2020-06-05 17:09:51 -0300

    Tratando Respostas e códigos de Retorno com o ResponseEntity

--- src/main/java/br/com/dominio/livros/api/controller/LivroController.java
+++ src/main/java/br/com/dominio/livros/api/controller/LivroController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +21,13 @@ public class LivroController {
 	private LivroService livroService;
 	
 	@GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE})
-	public List<Livro> listarLivros() {
+	public ResponseEntity<List<Livro>> listarLivros() {
 		return livroService.listarLivros();
 	}
+	
+	@GetMapping(path="/{id}", produces = { MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<Livro> buscarLivros(@PathVariable int id) {
+		return livroService.buscarLivros(id);
+	}
+	
 }
--- src/main/java/br/com/dominio/livros/api/service/LivroService.java
+++ src/main/java/br/com/dominio/livros/api/service/LivroService.java
@@ -4,6 +4,9 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.catalina.connector.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.dominio.livros.api.model.Livro;
@@ -11,7 +14,7 @@ import br.com.dominio.livros.api.model.Livro;
 @Service
 public class LivroService {
 
-	public List<Livro> listarLivros() {
+	public ResponseEntity<List<Livro>> listarLivros() {
 		List<Livro> livros = new ArrayList<>();
 		Livro l1 = new Livro(1, "Spring Total", LocalDate.of(2020, 5, 15));
 		Livro l2 = new Livro(2, "Java total", LocalDate.of(2019, 2, 15));
@@ -19,7 +22,15 @@ public class LivroService {
 		livros.add(l1);
 		livros.add(l2);
 		livros.add(l3);
-		return livros;
+		return ResponseEntity.ok(livros);
 	}
 
+	public ResponseEntity<Livro> buscarLivros(Integer id) {
+		Livro l1 = new Livro(1, "Spring Total", LocalDate.of(2020, 5, 15));
+		if (id.equals(1)) {
+			return ResponseEntity.ok(l1);
+		} else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		}
+	}
 }
